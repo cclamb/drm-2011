132d2cb55984ae6ed91c36b614cfee25fa602ddb
cclamb <cclamb@cs.unm.edu>
2011-07-03 21:54:57 -0600

    new stuff from mat

--- content/code/cc/restrictedactivities.rb
+++ content/code/cc/restrictedactivities.rb
@@ -5,37 +5,32 @@ end
 
 # 2) Attribution
 share_by_work = restrict share do
-  with attribution_constraint
+  with attribution
 end
 
 # 3) Attribution, Share-Alike
 share_by_sa_work = restrict share do
-  with attribution_constraint,
-       share_alike_constraint
+  with attribution, share_alike
 end
 
 # 4) Attribution, No-Derivatives
 share_by_nd_work = restrict share do
-  with attribution_constraint,
-       non_derivative_work_constraint
+  with attribution, non_derivative_work
 end
 
 # 5) Attribution, Non-Commercial
 share_by_nc_work = restrict share do
-  with attribution_constraint,
-       non_commercial_use_constraint
+  with attribution, non_commercial_use
 end
 
 # 6) Attribution, Non-Commercial, Share-Alike
 share_by_nc_sa_work = restrict share do
-  with attribution_constraint,
-       non_commercial_use_constraint,
-       share_alike_constraint
+  with attribution, non_commercial_use,
+       share_alike
 end
 
 # 7) Atribution, Non-Commercial, No-Derivatives
 share_by_nc_nd_work = restrict share do
-  with attribution_constraint,
-       non_commercial_use_constraint,
-       non_derivative_work_constraint
+  with attribution, non_commercial_use,
+       non_derivative_work
 end
